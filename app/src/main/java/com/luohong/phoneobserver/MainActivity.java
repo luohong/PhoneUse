05096d76f816b5ebd56527e1ef89f4dc1c90a4a2
luohong722 <luohong722@yahoo.cn>
2015-09-22 01:34:31 +0800

    更新选中周几的文字背景改变效果

--- app/src/main/java/com/luohong/phoneobserver/MainActivity.java
+++ app/src/main/java/com/luohong/phoneobserver/MainActivity.java
@@ -2,82 +2,102 @@ package com.luohong.phoneobserver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView monday;
-    private TextView tuesday;
-    private TextView wednesday;
-    private TextView thursday;
-    private TextView friday;
-    private TextView saturday;
-    private TextView sunday;
+    private TextView tvMonday;
+    private TextView tvTuesday;
+    private TextView tvWednesday;
+    private TextView tvThursday;
+    private TextView tvFriday;
+    private TextView tvSaturday;
+    private TextView tvSunday;
     private ImageView ivMonday;
+    private ImageView ivTuesday;
+    private ImageView ivWednesday;
+    private ImageView ivThursday;
+    private ImageView ivFriday;
+    private ImageView ivSaturday;
+    private ImageView ivSunday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        monday = (TextView) findViewById(R.id.tv_monday);
+        tvMonday = (TextView) findViewById(R.id.tv_monday);
         ivMonday = (ImageView) findViewById(R.id.iv_monday);
-        tuesday = (TextView) findViewById(R.id.tv_tuesday);
-        wednesday = (TextView) findViewById(R.id.tv_wednesday);
-        thursday = (TextView) findViewById(R.id.tv_thursday);
-        friday = (TextView) findViewById(R.id.tv_friday);
-        saturday = (TextView) findViewById(R.id.tv_saturday);
-        sunday = (TextView) findViewById(R.id.tv_sunday);
+        tvTuesday = (TextView) findViewById(R.id.tv_tuesday);
+        ivTuesday = (ImageView) findViewById(R.id.iv_tuesday);
+        tvWednesday = (TextView) findViewById(R.id.tv_wednesday);
+        ivWednesday = (ImageView) findViewById(R.id.iv_wednesday);
+        tvThursday = (TextView) findViewById(R.id.tv_thursday);
+        ivThursday = (ImageView) findViewById(R.id.iv_thursday);
+        tvFriday = (TextView) findViewById(R.id.tv_friday);
+        ivFriday = (ImageView) findViewById(R.id.iv_friday);
+        tvSaturday = (TextView) findViewById(R.id.tv_saturday);
+        ivSaturday = (ImageView) findViewById(R.id.iv_saturday);
+        tvSunday = (TextView) findViewById(R.id.tv_sunday);
+        ivSunday = (ImageView) findViewById(R.id.iv_sunday);
 
 
     }
 
     public void daySelect(View view) {
 
-        monday.setSelected(false);
+        tvMonday.setSelected(false);
         ivMonday.setSelected(false);
-        tuesday.setSelected(false);
-        wednesday.setSelected(false);
-        thursday.setSelected(false);
-        friday.setSelected(false);
-        saturday.setSelected(false);
-        sunday.setSelected(false);
+        tvTuesday.setSelected(false);
+        ivTuesday.setSelected(false);
+        tvWednesday.setSelected(false);
+        ivWednesday.setSelected(false);
+        tvThursday.setSelected(false);
+        ivThursday.setSelected(false);
+        tvFriday.setSelected(false);
+        ivFriday.setSelected(false);
+        tvSaturday.setSelected(false);
+        ivSaturday.setSelected(false);
+        tvSunday.setSelected(false);
+        ivSunday.setSelected(false);
 
         switch (view.getId()) {
 
             case R.id.tv_monday:
-                monday.setSelected(true);
+                tvMonday.setSelected(true);
                 ivMonday.setSelected(true);
 
                 break;
             case R.id.tv_tuesday:
-                tuesday.setSelected(true);
+                tvTuesday.setSelected(true);
+                ivTuesday.setSelected(true);
 
                 break;
             case R.id.tv_wednesday:
-                wednesday.setSelected(true);
+                tvWednesday.setSelected(true);
+                ivWednesday.setSelected(true);
 
                 break;
             case R.id.tv_thursday:
-                thursday.setSelected(true);
+                tvThursday.setSelected(true);
+                ivThursday.setSelected(true);
 
                 break;
             case R.id.tv_friday:
-                friday.setSelected(true);
+                tvFriday.setSelected(true);
+                ivFriday.setSelected(true);
 
                 break;
             case R.id.tv_saturday:
-                saturday.setSelected(true);
+                tvSaturday.setSelected(true);
+                ivSaturday.setSelected(true);
 
                 break;
 
             case R.id.tv_sunday:
-                sunday.setSelected(true);
+                tvSunday.setSelected(true);
+                ivSunday.setSelected(true);
 
                 break;
 
