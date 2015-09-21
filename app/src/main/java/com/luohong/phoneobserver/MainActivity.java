3ec380bc7deaf85aea0a65bb13e36f7c58d27661
luohong722 <luohong722@yahoo.cn>
2015-09-22 00:14:29 +0800

    更新包名phoneobserver

--- app/src/main/java/com/luohong/phoneobserver/MainActivity.java
+++ app/src/main/java/com/luohong/phoneobserver/MainActivity.java
@@ -4,34 +4,86 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView monday;
+    private TextView tuesday;
+    private TextView wednesday;
+    private TextView thursday;
+    private TextView friday;
+    private TextView saturday;
+    private TextView sunday;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         monday = (TextView) findViewById(R.id.tv_monday);
+        tuesday = (TextView) findViewById(R.id.tv_tuesday);
+        wednesday = (TextView) findViewById(R.id.tv_wednesday);
+        thursday = (TextView) findViewById(R.id.tv_thursday);
+        friday = (TextView) findViewById(R.id.tv_friday);
+        saturday = (TextView) findViewById(R.id.tv_saturday);
+        sunday = (TextView) findViewById(R.id.tv_sunday);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void daySelect(View view) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        monday.setSelected(false);
+        tuesday.setSelected(false);
+        wednesday.setSelected(false);
+        thursday.setSelected(false);
+        friday.setSelected(false);
+        saturday.setSelected(false);
+        sunday.setSelected(false);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (view.getId()) {
+
+            case R.id.tv_monday:
+                monday.setSelected(true);
+
+                break;
+            case R.id.tv_tuesday:
+                tuesday.setSelected(true);
+                tuesday.setBackgroundColor(0x4DB85A);
+
+                break;
+            case R.id.tv_wednesday:
+                wednesday.setSelected(true);
+                wednesday.setBackgroundColor(0x4DB85A);
+
+                break;
+            case R.id.tv_thursday:
+                thursday.setSelected(true);
+                thursday.setBackgroundColor(0x4DB85A);
+
+                break;
+            case R.id.tv_friday:
+                friday.setSelected(true);
+                friday.setBackgroundColor(0x4DB85A);
+
+                break;
+            case R.id.tv_saturday:
+                saturday.setSelected(true);
+                saturday.setBackgroundColor(0x4DB85A);
+
+                break;
+
+            case R.id.tv_sunday:
+                sunday.setSelected(true);
+                sunday.setBackgroundColor(0x4DB85A);
+
+                break;
+
+            default:
+                break;
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
